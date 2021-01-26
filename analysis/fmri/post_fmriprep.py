25742ad6352e79974d8391a282e25bc4aa9d7cdd
iverissimo <verissimo.iness@gmail.com>
2021-01-26 20:32:09 +0100

    post fmriprep processing script

---
+++ analysis/fmri/post_fmriprep.py
@@ -0,0 +1,80 @@
+
+# extra processing after fmriprep, for all tasks
+# outputs relevant files in derivatives folder
+# for further analysis
+
+import os, yaml
+import sys, glob
+import re 
+
+import numpy as np
+import pandas as pd
+
+from utils import * #import script to use relevante functions
+
+
+# load settings from yaml
+with open(os.path.join(os.path.split(os.getcwd())[0],'params.yml'), 'r') as f_in:
+            params = yaml.safe_load(f_in)
+
+
+# define participant number and open json parameter file
+if len(sys.argv)<2:
+    raise NameError('Please add subject number (ex:01) or "all" '
+                    'as 1st argument in the command line!')
+    
+else:
+    if sys.argv[1] == 'all': # process all subjects  
+        sj = ['sub-'+str(x).zfill(2) for x in params['general']['subs']]
+    else:     
+        sj = ['sub-'+str(sys.argv[1]).zfill(2)] #fill subject number with 0 in case user forgets
+
+
+# define paths and list of files
+deriv_pth = params['general']['paths']['data']['derivatives'] # path to derivatives folder
+fmriprep_pth = os.path.join(deriv_pth,'fmriprep') # path to fmriprep files
+
+# run for all tasks
+for t,cond in enumerate(params['general']['tasks']):
+    
+    # and all subjects selected
+    for _,s in enumerate(sj):
+    
+        # list of functional files
+        filename = [run for run in glob.glob(os.path.join(fmriprep_pth,s,'*','func/*')) if cond in run and params['processing']['space'] in run and run.endswith(params['processing']['extension'])]
+        filename.sort()
+        
+        if not filename: # if list empty
+            print('Subject %s has no files for %s' %(s,cond))
+            
+        else:
+    
+            TR = params['general']['TR']
+
+            # set output path for processed files
+            out_pth = os.path.join(deriv_pth,'post_fmriprep',cond,s)
+            
+            if not os.path.exists(out_pth): # check if path to save processed files exist
+                os.makedirs(out_pth) 
+                
+            for _,file in enumerate(filename):
+
+                # define hemisphere to plot
+                hemi = 'left' if '_hemi-L' in file else 'right'
+                
+                if cond in ('prf'): # if pRF we cut out first 7TRs from "raw file" to make further analysis better
+
+                    file = crop_gii(file,params['processing']['crop_pRF_TR'],out_pth,extension = params['processing']['extension'])
+                
+                # high pass filter all runs (savgoy-golay)
+                filt_gii,filt_gii_pth = highpass_gii(file,params['processing']['sg_filt_polyorder'],params['processing']['sg_filt_deriv'],
+                                                         params['processing']['sg_filt_window_length'],out_pth, extension = params['processing']['extension'])
+
+                
+                # do PSC
+                psc_data, psc_data_pth = psc_gii(filt_gii_pth, out_pth, method = 'median', extension = params['processing']['extension']) 
+
+                # smooth it
+                smt_file, smt_pth = smooth_gii(psc_data_pth, out_pth, fwhm = params['processing']['smooth_fwhm'], extension = params['processing']['extension'])
+
+
--- analysis/fmri/utils.py
+++ analysis/fmri/utils.py
@@ -4,7 +4,15 @@
 import os, re
 import numpy as np
 
+import shutil
+
 import nibabel as nb
+
+from nilearn import surface
+
+from scipy.signal import savgol_filter
+import nipype.interfaces.freesurfer as fs
+
 
 
 def median_gii(files,outdir):
@@ -45,6 +53,231 @@ def median_gii(files,outdir):
     return median_file
 
 
+def crop_gii(gii_path,num_TR,outpath, extension = '.func.gii'):
+
+    """ crop gii file
+
+    Parameters
+    ----------
+    gii_path : str
+        absolute filename for gii file
+    num_TR : int
+        number of TRs to remove from beginning of file
+    outpath : str
+        path to save new file
+    extension: str
+        file extension
+    
+
+    Outputs
+    -------
+    crop_gii_path: str
+        absolute filename for cropped gii file
+    
+    """
+
+    # load with nibabel instead to save outputs always as gii
+    gii_in = nb.load(gii_path)
+    data_in = np.array([gii_in.darrays[i].data for i in range(len(gii_in.darrays))]) #load surface data
+
+    crop_data = data_in[num_TR:,:] # crop initial TRs
+    print('original file with %d TRs, now cropped and has %d TRs' %(data_in.shape[0],crop_data.shape[0]))
+
+    # save as gii again
+    darrays = [nb.gifti.gifti.GiftiDataArray(d) for d in crop_data]
+    gii_out = nb.gifti.gifti.GiftiImage(header = gii_in.header, extra = gii_in.extra, darrays = darrays)
+
+    crop_gii_path = os.path.join(outpath,os.path.split(gii_path)[-1].replace(extension,'_cropped'+extension))
+
+    nb.save(gii_out,crop_gii_path) # save as gii file
+    print('new file saved in %s'%crop_gii_path)
+
+    return crop_gii_path
+
+
+
+def highpass_gii(filename,polyorder,deriv,window,outpth, extension = '.func.gii'):
+
+    """ highpass filter gii file
+
+    Parameters
+    ----------
+    filename : List/array
+        list of absolute filename for gii file
+    polyorder : int
+        order of the polynomial used to fit the samples - must be less than window_length.
+    deriv : int
+        order of the derivative to compute - must be a nonnegative integer
+    window: int
+        length of the filter window (number of coefficients) - must be a positive odd integer
+    outpth: str
+        path to save new files
+    extension: str
+        file extension
+    
+
+    Outputs
+    -------
+    filename_sg: arr
+        np array with filtered run
+    filepath_sg: str
+        filtered filename
+    
+    """
+
+
+    if not os.path.isfile(filename): # check if file exists
+            print('no file found called %s' %filename)
+            filename_sg = []
+            filepath_sg = []
+    else:
+
+        # load with nibabel instead to save outputs always as gii
+        gii_in = nb.load(filename)
+        data_in = np.array([gii_in.darrays[i].data for i in range(len(gii_in.darrays))]) #load surface data
+
+        print('filtering run %s' %filename)
+        data_in_filt = savgol_filter(data_in, window, polyorder, axis = 0, deriv = deriv, mode = 'nearest')
+        filename_sg = data_in - data_in_filt + data_in_filt.mean(axis = 0) # add mean image back to avoid distribution around 0
+
+        darrays = [nb.gifti.gifti.GiftiDataArray(d) for d in filename_sg]
+        gii_out = nb.gifti.gifti.GiftiImage(header = gii_in.header, extra = gii_in.extra, darrays = darrays)
+
+        filepath_sg = os.path.join(outpth,os.path.split(filename)[-1].replace(extension,'_sg'+extension))
+
+        nb.save(gii_out,filepath_sg) # save as gii file
+
+
+    return np.array(filename_sg),filepath_sg
+
+
+
+def psc_gii(gii_file,outpth, method='median', extension = '.func.gii'):
+
+    """ percent signal change gii file
+
+    Parameters
+    ----------
+    gii_file : str
+        absolute filename for gii
+    outpth: str
+        path to save new files
+    method: str
+        do median vs mean 
+    extension: str
+        file extension
+
+    Outputs
+    -------
+    psc_gii: arr
+        np array with percent signal changed file
+    psc_gii_pth: List/arr
+        list with absolute filenames for saved giis
+    
+    """
+
+
+    if not os.path.isfile(gii_file): # check if file exists
+            print('no file found called %s' %gii_file)
+            psc_gii = []
+            psc_gii_pth = []
+    else:
+
+        # load with nibabel instead to save outputs always as gii
+        img_load = nb.load(gii_file)
+        data_in = np.array([img_load.darrays[i].data for i in range(len(img_load.darrays))]) #load surface data
+
+        print('PSC run %s' %gii_file)
+
+        if method == 'mean':
+            data_m = np.mean(data_in,axis=0)
+        elif method == 'median':
+            data_m = np.median(data_in, axis=0)
+
+        psc_gii = 100.0 * (data_in - data_m)/data_m#np.abs(data_m)
+
+        new_name =  os.path.split(gii_file)[-1].replace(extension,'_psc'+extension) # file name
+
+        darrays = [nb.gifti.gifti.GiftiDataArray(d) for d in psc_gii]
+        new_gii = nb.gifti.gifti.GiftiImage(header = img_load.header,
+                                           extra = img_load.extra,
+                                           darrays = darrays) # need to save as gii again
+        psc_gii_pth = os.path.join(outpth,new_name)
+
+        print('saving %s' %psc_gii_pth)
+        nb.save(new_gii,psc_gii_pth) #save in correct path
+
+
+
+    return np.array(psc_gii),psc_gii_pth
+
+
+def smooth_gii(gii_file, outdir, fwhm = 5, extension = '.func.gii'):
+
+    """ percent signal change gii file
+
+    Parameters
+    ----------
+    gii_file : str
+        absolute filename for gii
+    outdir: str
+        path to save new files
+    fwhm: int
+        width of the kernel, at half of the maximum of the height of the Gaussian
+    extension: str
+        file extension
+
+    Outputs
+    -------
+    smooth_gii: arr
+        np array with smoothed file
+    smooth_gii_pth: str
+        absolute path for smoothed file
+    
+    """
+
+    if not os.path.isfile(gii_file): # check if file exists
+            print('no file found called %s' %gii_file)
+            smooth_gii = []
+            smooth_gii_pth = []
+    else:
+
+        # load with nibabel instead to save outputs always as gii
+        gii_in = nb.load(gii_file)
+        data_in = np.array([gii_in.darrays[i].data for i in range(len(gii_in.darrays))]) #load surface data
+
+        print('loading file %s' %gii_file)
+
+        # first need to convert to mgz
+        # will be saved in output dir
+        new_mgz = os.path.join(outdir,os.path.split(gii_file)[-1].replace(extension,'.mgz'))
+
+        print('converting gifti to mgz as %s' %(new_mgz))
+        os.system('mri_convert %s %s'%(gii_file,new_mgz))
+
+        # now smooth it
+        smoother = fs.SurfaceSmooth()
+        smoother.inputs.in_file = new_mgz
+        smoother.inputs.subject_id = 'fsaverage'
+
+        # define hemisphere
+        smoother.inputs.hemi = 'lh' if '_hemi-L' in new_mgz else 'rh'
+        print('smoothing %s' %smoother.inputs.hemi)
+        smoother.inputs.fwhm = fwhm
+        smoother.run() # doctest: +SKIP
+
+        new_filename = os.path.split(new_mgz)[-1].replace('.mgz','_smooth%d.mgz'%(smoother.inputs.fwhm))
+        smooth_mgz = os.path.join(outdir,new_filename)
+        shutil.move(os.path.join(os.getcwd(),new_filename), smooth_mgz) #move to correct dir
+
+        # transform to gii again
+        smooth_gii = surface.load_surf_data(smooth_mgz).T
+
+        smooth_gii_pth = smooth_mgz.replace('.mgz',extension)
+        print('converting to %s' %smooth_gii_pth)
+        os.system('mri_convert %s %s'%(smooth_mgz,smooth_gii_pth))
+
+    return np.array(smooth_gii),smooth_gii_pth
 
 
 
