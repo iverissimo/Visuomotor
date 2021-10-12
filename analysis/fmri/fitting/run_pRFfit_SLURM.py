133444c95363275c9b9b772db61958324e4979ab
iverissimo <verissimo.iness@gmail.com>
2021-10-12 16:18:15 +0200

    redifined according to new script

--- analysis/fmri/fitting/run_pRFfit_SLURM.py
+++ analysis/fmri/fitting/run_pRFfit_SLURM.py
@@ -4,70 +4,80 @@
 # (to use in cartesius or similar server)
 ################################################
 
-import re, os
-import glob, yaml
+import os
+import os.path as op
+import yaml
 import sys
 
 # load settings from yaml
 with open(os.path.join(os.path.split(os.path.split(os.getcwd())[0])[0],'params.yml'), 'r') as f_in:
     params = yaml.safe_load(f_in)
 
-# define participant number 
-if len(sys.argv)<2:
-    raise NameError('Please add subject number (ex:01) or "all" '
-                    'as 1st argument in the command line!')
-    
-else:
-    if sys.argv[1] == 'all': # process all subjects  
-        subjects = [str(x) for x in params['general']['subs']]
-    else:     
-        subjects = [str(sys.argv[1])] # make it string
-
+subjects = ['01','02','04','08','09','11','12'] # subjects
+base_dir = 'lisa' # where we are running the scripts
 # number of chunks to split data in (makes fitting faster)
 total_chunks = params['fitting']['prf']['total_chunks']
 
+loo_runs = ['leave_01_out','leave_02_out','leave_03_out','leave_04_out','leave_05_out']
+
 batch_string = """#!/bin/bash
 #SBATCH -t 100:00:00
-#SBATCH -N 1
+#SBATCH -N 1 --mem=65536
 #SBATCH -v
 #SBATCH -c 24
+#SBATCH --output=$BATCHDIR/slurm_PRF-%A.out
 
 # call the programs
 echo "Job $SLURM_JOBID started at `date`" | mail $USER -s "Job $SLURM_JOBID"
 
 conda activate i36
 
-python $PY_CODE $SJ_NR $CHUNK_NR
+cp -r $DATADIR/ $TMPDIR
+
+wait
+
+python $PY_CODE $SJ_NR $CHUNK_NR $RUN_TYPE
 
 wait          # wait until programs are finished
+
+rsync -chavzP $TMPDIR/$DATADIR / $DATADIR
+
+wait
 
 echo "Job $SLURM_JOBID finished at `date`" | mail $USER -s "Job $SLURM_JOBID"
 """
 
-# base directory in cartesius
-base_dir = params['general']['paths']['fitting']['cartesius']['basedir']
+# base directory 
+base_dir = params['general']['paths']['fitting']['lisa']['basedir']
 # to save .sh files, for each chunk
-batch_dir = os.path.join(base_dir,'batch')
+batch_dir = op.join(base_dir,'batch')
 # path to python fitting script  
-pycode_dir = os.path.join(base_dir,'Visuomotor','analysis','fmri','fitting','pRF_fitmodel.py')
+pycode_dir = op.join(base_dir,'Visuomotor','analysis','fmri','fitting','pRF_fitmodel.py')
+# directory with all data
+data_dir = params['general']['paths']['fitting']['lisa']['data_dir']
 
-os.chdir(batch_dir)
+#os.chdir(batch_dir)
 
 
 for sub in subjects:
 
-    for _,chu in enumerate(range(total_chunks)): # submit job for each chunk
+    for rt in loo_runs:
 
-        working_string = batch_string.replace('$SJ_NR', str(sub).zfill(2))
-        working_string = working_string.replace('$CHUNK_NR', str(chu+1).zfill(3))
-        working_string = working_string.replace('$PY_CODE', pycode_dir)
+        for _,chu in enumerate(range(total_chunks)): # submit job for each chunk
 
-        js_name = os.path.join(batch_dir, 'pRF_sub-' + str(sub).zfill(2) + '_chunk-%s_of_%s'%(str(chu+1).zfill(3),str(total_chunks).zfill(3)) + '_iterative.sh')
-        of = open(js_name, 'w')
-        of.write(working_string)
-        of.close()
+            working_string = batch_string.replace('$SJ_NR', str(sub).zfill(2))
+            working_string = working_string.replace('$CHUNK_NR', str(chu+1).zfill(3))
+            working_string = working_string.replace('$PY_CODE', pycode_dir)
+            working_string = working_string.replace('$BATCHDIR', batch_dir)
+            working_string = working_string.replace('$RUN_TYPE', rt)
+            working_string = working_string.replace('$DATADIR', data_dir)
 
-        print('submitting ' + js_name + ' to queue')
-        print(working_string)
-        os.system('sbatch ' + js_name)
+            js_name = os.path.join(batch_dir, 'pRF_sub-' + str(sub).zfill(2) + '_chunk-%s_of_%s'%(str(chu+1).zfill(3),str(total_chunks).zfill(3)) + '_iterative.sh')
+            of = open(js_name, 'w')
+            of.write(working_string)
+            of.close()
+
+            print('submitting ' + js_name + ' to queue')
+            print(working_string)
+            os.system('sbatch ' + js_name)
 
